6a18023af0de3d0b7a158a53bbdf2fd6eb7099ae
sagarbindu <dashsagarbindu789@gmail.com>
2026-03-18 11:51:44 +0530

    added prometheus metrics

--- index.js
+++ index.js
@@ -1,8 +1,39 @@
-const express = require('express');
+const express = require("express");
+const client = require("prom-client");
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('DevOps Project Running 🚀');
+// Create a Registry
+const register = new client.Registry();
+
+// Default metrics (CPU, memory, etc.)
+client.collectDefaultMetrics({ register });
+
+// Custom metric
+const httpRequestCounter = new client.Counter({
+  name: "http_requests_total",
+  help: "Total number of HTTP requests",
 });
 
-app.listen(3000, () => console.log('Server running on port 3000'));
+register.registerMetric(httpRequestCounter);
+
+// Middleware
+app.use((req, res, next) => {
+  httpRequestCounter.inc();
+  next();
+});
+
+// Your app route
+app.get("/", (req, res) => {
+  res.send("Hello DevOps 🚀");
+});
+
+// Metrics endpoint (IMPORTANT 🔥)
+app.get("/metrics", async (req, res) => {
+  res.set("Content-Type", register.contentType);
+  res.end(await register.metrics());
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
